6059aaab290b4d135b4eaf429799860b0ba0052b
st974 <st974@drexel.edu>
2020-04-01 22:50:34 -0400

    Added a directory for the tic-tac-toe assignment and implemented most of the functions in tictactoe.py

---
+++ project-0/tic-tac-toe/tictactoe.py
@@ -0,0 +1,156 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+
+X = "X"
+O = "O"
+BOARD_LENGTH = 3
+BOARD_WIDTH = 3
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    # We will figure out who's turn it is by counting the number
+    # of X's and O's, and then seeing if that number is even or odd
+    xFreq = 0
+    yFreq = 0
+
+    for row in board:
+        for square in row:
+            if square == "X":
+                xFreq += 1
+            elif square == "O":
+                yFreq += 1
+    
+    movesPlayed = xFreq + yFreq
+    if movesPlayed % 2 == 0:
+        return "X"
+    else:
+        return "O"
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    # We'll get all the legal moves by iterating through the whole board
+    # and checking if the value of the board is set to EMPTY or not
+    possibleActions = set()
+    for i in range(BOARD_LENGTH):
+        for j in range (BOARD_WIDTH):
+            if board[i][j] == EMPTY:
+                possibleActions.add((i, j))
+
+    return possibleActions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    # We'll return the resulting board by first making a copy of the board, then
+    # checking that the square is not already occupied
+    newBoard = board[:]
+    
+    # Check validity of action
+    if newBoard[action[0]][action[1]] != EMPTY:
+        raise UserWarning("Invalid move")
+    else:
+        newBoard[action[0]][action[1]] = player(board)
+
+    return newBoard
+
+
+def checkWin(board, win):
+    """
+    Returns the winner of the game, given a board and a winning combination to look for.
+    """
+    # To figure out if the winning combination is present, we'll look at each of the 3
+    # sqaures and see if they are occupied by the same piece
+    firstSquare = board[win[0][0]][win[0][1]]
+    secondSquare = board[win[1][0]][win[1][1]]
+    thirdSquare = board[win[2][0]][win[2][1]]
+    
+    if firstSquare == "X" and secondSquare == "X" and thirdSquare == "X":
+        return "X"
+    elif firstSquare == "O" and secondSquare == "O" and thirdSquare == "O":
+        return "O"
+    else:
+        return None
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    # To figure out who won the game, we'll iterate over all 8 possibilities,
+    # checking if any of the winning possibilities are present in the board 
+    wins = [[(0, 0), (0, 1), (0, 2)], [(1, 0), (1, 1), (1, 2)], 
+            [(2, 0), (2, 1), (2, 2)], [(0, 0), (1, 0), (2, 0)], 
+            [(0, 1), (1, 1), (2, 1)], [(0, 2), (1, 2), (2, 2)], 
+            [(0, 0), (1, 1), (2, 2)], [(0, 2), (1, 1), (2, 0)]]
+
+    for win in wins:
+         result = checkWin(board, win)
+         if result != None:
+             return result
+    
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    # To determine if the game is over, we'll first check if our winner
+    # function returns a player. If not, we'll see if there are any unoccupied
+    # squares left on the board. If not, the game is over. 
+    if winner(board) != None:
+        return True
+
+    numOccupiedSquares = 0
+    for i in range(BOARD_LENGTH):
+        for j in range(BOARD_WIDTH):
+            if board[i][j] != EMPTY:
+                numOccupiedSquares += 1
+
+    if numOccupiedSquares == 9:
+        return True
+    else:
+        return False    
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    # Determine the winner by using the winner function as a helper
+    winningPlayer = winner(board)
+
+    if winningPlayer == "X":
+        return 1
+    elif winningPlayer == "O":
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    
+    raise NotImplementedError
